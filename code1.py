cbd11e1363cc763becc1152b9817620aa25f032f
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-07-19 15:47:38 +0530

    test

--- code1.py
+++ code1.py
@@ -1,2 +1,10 @@
-age = int(input())
-print("I am %s years old" %(age))
+#age = int(input())
+#print("I am %s years old" %(age))
+
+str = [pyhton]
+#x = len(str)
+#print(x)
+count = 0
+for x in str:
+    count = count + 1
+print (count)
